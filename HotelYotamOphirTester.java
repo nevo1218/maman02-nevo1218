7b00566171ce07bc01ab893c2c106b6c4c5f55ad
baraksu <baraksu@gmail.com>
2026-01-13 23:19:08 +0200

    Sync HotelYotamOphirTester.java from maman02-YotamOphir

---
+++ HotelYotamOphirTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelYotamOphirTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
