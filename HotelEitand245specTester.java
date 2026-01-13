eb5251b540a703fc204f66c28480ed1f547ef4ce
baraksu <baraksu@gmail.com>
2026-01-13 23:16:58 +0200

    Sync HotelEitand245specTester.java from maman02-eitand245-spec

---
+++ HotelEitand245specTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelEitand245specTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
