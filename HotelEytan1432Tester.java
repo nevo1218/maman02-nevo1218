27fa3770f14c0d23a40123451df8c23a5bd98485
baraksu <baraksu@gmail.com>
2026-01-13 23:17:28 +0200

    Sync HotelEytan1432Tester.java from maman02-eytan1432

---
+++ HotelEytan1432Tester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelEytan1432Tester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
